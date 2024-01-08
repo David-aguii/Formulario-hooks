195ac6873772cf2172d59e868fa0fd2d63795b73
David-aguii <davidaguii1927@gmail.com>
2024-01-08 19:48:20 -0300

    Formulario hooks

--- nombre_del_proyecto/src/Componente/Formulario.jsx
+++ nombre_del_proyecto/src/Componente/Formulario.jsx
@@ -4,6 +4,7 @@ const InputComponent= ({ label , type , state , setState })=>{
     return(
         <div className= {styles.inputElement}>
             <label htmlFor={`${label}Input`}>{label}</label>:
+ 
             <input  
                 type={type}
                 id={`${label}Input`}
@@ -14,53 +15,35 @@ const InputComponent= ({ label , type , state , setState })=>{
         </div>
     )
 }
-
 const Formulario= ()=>{
     const [firstName,setFirstName]=useState("");
     const [lastName,setLastName]=useState("");
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
     const [confirmPassword,setConfirmPassword]=useState("");
- 
-    const ValidacionesName=()=>{
-        if(firstName.length >= 1 && firstName.length <= 2 ){
-            return <h6>First Name must be at least 2 characters</h6>
-        }    
-    }
-    const ValidacionLastName=()=>{
-        if(lastName.length >= 1 && lastName.length <= 2 ){
-            return <h6>Last name must be at least 2 characters</h6>
-        }
-    } 
-    const ValidacionEmail=()=>{
-        if(email.length >= 1 && email.length <= 5 ){
-            return <h6>Email must be at least 5 characters</h6>
-        }
-    }
-    const ValidacionPassword=()=>{
-        if((password.length >= 1 && password.length <= 8 ) || (password !== confirmPassword)){
-            return <h6>Password must be at least 8 characters <hr/> Passwords must match</h6>
-        }
-    }
     return(
         <header>
             <div action="#" className={styles.formulario}>
                 <InputComponent label="First name" type="text" state={firstName} setState={setFirstName}/>
-                <ValidacionesName/>
                 <InputComponent label="Last name" type="text" state={lastName} setState={setLastName}/>
-                <ValidacionLastName/>
                 <InputComponent label="Email" type="text" state={email} setState={setEmail}/>    
-                <ValidacionEmail/>
                 <InputComponent label="Password" type="password" state={password} setState={setPassword}/>
-                <ValidacionPassword/>
                 <InputComponent label="Confirm Password" type="password" state={confirmPassword} setState={setConfirmPassword}/>
-                <ValidacionPassword/>
             </div>
-           
+            <hr/>
+            <div className={styles.datos}>
+                <h2>YOUR FORM DATA</h2>
+                <ul>
+                    <li>First name: {firstName}</li>
+                    <li>Last name: {lastName}</li>
+                    <li>Email: {email}</li>
+                    <li>Password: {password}</li>
+                    <li>Confirm password: {confirmPassword}</li>
+                </ul>
+            </div>
         </header>
     )
 }
 
- 
 export default Formulario;
 
